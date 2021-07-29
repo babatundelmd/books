443c1e4a898ddc8133bdaff6372abfb5332d8114
Babatunde Lamidi <odunayohlamidi@gmail.com>
2021-07-29 23:15:28 +0100

    books init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddBookComponent } from './books/add-book/add-book.component';
+import { BooksComponent } from './books/books.component';
+import { UpdateBookComponent } from './books/update-book/update-book.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BooksComponent },
+  { path: 'update/:id', component: UpdateBookComponent },
+  { path: 'add-book', component: AddBookComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BooksComponent } from './books/books.component';
+import { AddBookComponent } from './books/add-book/add-book.component';
+import { UpdateBookComponent } from './books/update-book/update-book.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BooksComponent,
+    AddBookComponent,
+    UpdateBookComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
